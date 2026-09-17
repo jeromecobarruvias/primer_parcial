cbb22c125867e1d27aa282617489c5ac1e201ed3
Jerome Covarrubias Mejia <jeromecobarruvias@gmail>
2026-09-17 13:19:37 -0600

    avance3

---
+++ funciones/conversion_tiempo.py
@@ -0,0 +1,6 @@
+def leer_segundos():
+    print("Se esta leyendo la cantidad de segundos")
+def convertir_tiempo():
+    print("Se esta convirtiendo los segundos a horas, minutos y segundos")
+def mostrar_tiempo():
+    print("Se esta mostrando el tiempo convertido")
