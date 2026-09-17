e4f5539a37fef95ad7bddacbf0862e1a8a05fbe7
Jerome Covarrubias Mejia <jeromecobarruvias@gmail>
2026-09-17 13:18:16 -0600

    avance2

---
+++ funciones/pago_semanal.py
@@ -0,0 +1,6 @@
+def leer_horas_tarifa():
+    print("Se esta leyendo las horas trabajadas y la tarifa por hora")
+def calcular_pago():
+    print("Se esta calculando el pago semanal")
+def mostrar_pago():
+    print("Se esta mostrando el pago semanal")
