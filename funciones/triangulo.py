3d917c9dcd486567d58c5d00b5d70ceef968d6b9
Jerome Covarrubias Mejia <jeromecobarruvias@gmail>
2026-09-17 13:47:22 -0600

    evidencia5

--- funciones/triangulo.py
+++ funciones/triangulo.py
@@ -1,5 +1,5 @@
 def leer_base_altura():
-    print("Se esta leyendo la base y la altura del triangulo")
+    print("Se  esta leyendo la base y la altura del triangulo")
 def calcular_area():
     print("Se esta calculando el area del triangulo")
 def mostrar_area():
